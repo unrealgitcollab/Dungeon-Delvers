095a2110e60f5134f79df9e397bab7630812010f
Mees Broer <meesbroer@gmail.com>
2018-06-09 18:22:16 +0200

    Revert "forgot add"
    This reverts commit 68bc18d1269c1d0cb24152b1c94c86aeb447cee5.

--- Source/DungeonDelvers/DungeonDelvers.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "DungeonDelvers.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DungeonDelvers, "DungeonDelvers" );
-
-DEFINE_LOG_CATEGORY(LogDungeonDelvers)
- 
